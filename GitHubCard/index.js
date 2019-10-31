19b1d527cc21cf4f3df8ffb20b6945911240d2cd
Austin Walela <austinmasibo@gmail.com>
2019-10-31 15:41:16 +0300

    implemented a get request to gh api and logged data

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -2,6 +2,14 @@
            (replacing the palceholder with your Github name):
            https://api.github.com/users/<your name>
 */
+axios
+  .get("https://api.github.com/users/walela")
+  .then(res => {
+    console.log(res.data);
+  })
+  .catch(err => {
+    console.error(err);
+  });
 
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
